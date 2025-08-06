b02ef80c4c2d99e4a4b86a67f84fd58999058784
ffuad13 <jhonatom99@gmail.com>
2025-08-06 22:38:22 +0800

    add todo CRUD and DB postgresql

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 
 //import router
 const router = require('./routes/router');
 const userRouter = require('./routes/userRouter')
+const todoRouter = require('./routes/todoRouter')
 
 //import custom middleware
-const {logger} = require('./middlewares/logger')
+const {logger} = require('./middlewares/logger');
 
+//inisiasi instance express dalam variabel app
 const app = express();
 
 //use middlewares
 app.use(logger);
+app.use(compression())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -20,5 +24,6 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.use('/', router);
 app.use('/api/user', userRouter)
+app.use('/api/todo', todoRouter)
 
 app.listen(process.env.SERVER_PORT, () => {console.log('Server Running')});
